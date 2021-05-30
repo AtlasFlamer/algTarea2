72404c21c960094b85c46194e8927fb5069796ea
4m0xicilina <arturoreyescaceres@gmail.com>
2021-05-30 13:40:25 -0400

    a

--- ej1.py
+++ ej1.py
@@ -1,4 +1,13 @@
 #!/bin/python
+import random
+a=[]
+for cont in range(0,100, 1):
+    a.append((int(random.randint(1,6))))
+    if a[cont] == 1:
+        print("Has perdido")
+        break
+
+print(a)
 
 
 
