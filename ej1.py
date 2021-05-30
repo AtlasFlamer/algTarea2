51be0d4621b221a9e06224c8c7ca5feda99f1b9c
benja robles <benjaa.robless@gmail.com>
2021-05-30 13:49:49 -0400

    añadir ganar

--- ej1.py
+++ ej1.py
@@ -1,8 +1,12 @@
 #!/bin/python
 import random
 a=[]
-for cont in range(0,100, 1):
+for cont in range(0,100):
     a.append((int(random.randint(1,6))))
+
+    if a[cont-1] == a[cont]:
+        print ( ">>",  a[cont], " ", a[cont-1], end = " " ) 
+
     if a[cont] == 1:
         print("Has perdido")
         break
