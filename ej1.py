97a8aa643e6c2bccbd1804d7165538840a76f335
benja robles <benjaa.robless@gmail.com>
2021-05-30 14:02:27 -0400

    completar ej1

--- ej1.py
+++ ej1.py
@@ -1,15 +1,18 @@
 #!/bin/python
 import random
 a=[]
-for cont in range(0,100):
+a.append(0)
+for cont in range(2):
     a.append((int(random.randint(1,6))))
 
     if a[cont-1] == a[cont]:
         print ( ">>",  a[cont], " ", a[cont-1], end = " " ) 
+        print()
 
     if a[cont] == 1:
         print("Has perdido")
         break
+a.remove(0)
 
 print(a)
 
