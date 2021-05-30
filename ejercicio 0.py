039df9074392dc620b6564a6e26045f640471b1c
benja robles <benjaa.robless@gmail.com>
2021-05-30 00:21:19 -0400

    añadir funciones

--- ejercicio 0.py
+++ ejercicio 0.py
@@ -1,17 +1,30 @@
+from copy import deepcopy
+
 def mostrar( m ):
-    for f in range(m):
-        for c in range(n):
-            print(tmp[f][c], end=" ")
-        print()
+    for f in range(len(m)):
+        for c in range(len(m[0])):
+            print  (m[f][c], end= " " )
+        print ()
+    print ()
 
 def nueva(n, m):
     tmp = []
     for f in range(m):
         tmp.append([])
         for c in range(n):
-            tmp[f].append(int(input("Ingrese un numero: ")))
+            tmp[f].append( 0  )
     return tmp
 
-matriz = nueva(2, 3)
+matriz = nueva(3,3)
 
-mostrar(matriz)
+
+def indentidad( orden ):
+    tmp = nueva( orden, orden)
+    for i in range( orden ):
+        tmp[i][i] = 1
+
+    return tmp
+       
+
+mostrar( indentidad ( 5 ))
+
